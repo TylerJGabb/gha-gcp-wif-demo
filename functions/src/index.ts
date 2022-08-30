7ec8ec6e59265d886f0b96934f1fe01273133dcf
Tyler Gabb <tylerjgabb@gmail.com>
2022-08-30 14:54:46 -0700

    hello with a little more :hand:

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,5 +3,7 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   const { name } = request.query;
   functions.logger.info("Hello logs!", { structuredData: true });
-  response.send(`Hello, '${name ?? "Sure, Not"}'. From Firebase!`);
+  response.send(
+    `Hello, with a little more '${name ?? "Sure, Not"}'. From Firebase!`
+  );
 });
