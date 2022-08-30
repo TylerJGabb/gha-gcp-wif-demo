b1cecc8b463bc6378264f5ec0a66b094a5d83dd7
Tyler Gabb <tylerjgabb@gmail.com>
2022-08-30 15:04:55 -0700

    hello with firestore

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,18 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+admin.initializeApp();
 
-export const helloWorld = functions.https.onRequest((request, response) => {
-  const { name } = request.query;
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send(
-    `Hello, with a little more '${name ?? "Sure, Not"}'. From Firebase!`
-  );
-});
+export const helloWorld = functions.https.onRequest(
+  async (request, response) => {
+    const { name } = request.query;
+    functions.logger.info("Hello logs!", { structuredData: true });
+
+    const result = await admin.firestore().collection("people").add({ name });
+
+    response.send(
+      `Hello, with a little more '${name ?? "Sure, Not"}'. Your id is ${
+        result.id
+      }!`
+    );
+  }
+);
